3afe1879523242fcdbaae2c94a3b7638d87870d0
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-08-01 16:46:53 +0200

    initial form and to do list

---
+++ script/script.js
@@ -0,0 +1,8 @@
+console.log('JS OK')
+
+/*TO DO LIST
+1. Riprendere il sistema dell'esercizio scorso e adattarlo.
+2. Mostrare il sistema nel DOM
+3. Collegare il nostro sistema agli input nell'HTML
+4. Creare il randomizzatore per il numero della carrozza e codice CP
+*/ 
